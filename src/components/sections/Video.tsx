4151afbb2f1510fab40f3f269b2ab4f866715483
김권희 <kimkwon429@naver.com>
2025-03-10 17:42:58 +0900

    동영상 추가 작업

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,10 +1,23 @@
 import Section from "@shared/Section";
 import classNames from "classnames/bind";
-import style from "./Section.module.scss";
+import styles from "./Video.module.scss";
 import { FC } from "react";
 
+const cx = classNames.bind(styles);
+
 const Video: FC = () => {
-  return <Section>Video</Section>;
+  return (
+    <Section className={cx("container")}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  );
 };
 
 export default Video;
